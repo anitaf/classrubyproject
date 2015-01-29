8709051bf0a71716b7b20cf38e582f48718b0075
anitaf <anita.fwright@gmail.com>
2015-01-29 09:56:22 -0500

    added shuffle

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,1 +1,9 @@
-student_shuffle.rb
+# Write a program that splits the class into teams of 2.
+#If there's an odd number of students the last group should be a group of 
+
+students = ['student1','student','student']
+
+puts students
+""
+student.shuffle!
+puts student
